a393da15dec7ecaad98ad26cc18abf29573edc23
Isaac Druin <isaac.druin@gmail.com>
2025-03-23 13:33:25 -0500

    add enhanced images, add dark-mode image to the home page

---
+++ src/lib/components/blocks/Home_Section.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let { title, body, image }: { title: string; body: string; image: any } = $props();
+</script>
+
+<div class="w-full px-8">
+	<div class="relative container mx-auto grid grid-cols-2 gap-8">
+		<div class="bg-brand-teal-light absolute left-1/2 h-3/4 w-10 -translate-x-1/2">
+			<p>divider</p>
+		</div>
+		<div class="prose">
+			<h2>{title}</h2>
+			<p>{body}</p>
+		</div>
+		<enhanced:img class="h-full min-h-[350px] object-cover" src={image} alt=""></enhanced:img>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+	import HomeSection from '$lib/components/blocks/Home_Section.svelte';
 	import Hero from '$lib/components/homepage/Hero.svelte';
+
+	// images
+	import section1Img from '$lib/assets/images/suit.jpg?enhanced';
 </script>
 
 <Hero></Hero>
+<HomeSection title="title" body="adsafasdfgafhsdfgsdfg" image={section1Img}></HomeSection>
