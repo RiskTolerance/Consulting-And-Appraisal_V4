43dce8f24ff54fce656d120f26c9ce353f2c50e4
Isaac Druin <isaac.druin@gmail.com>
2025-03-29 21:24:41 -0500

    continue to add older content

---
+++ src/lib/components/cards/Ideas_PrimaryImageCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import SectionHeader from '../blocks/SectionHeader.svelte';
+	import IdeasPrimaryImageCard from '$lib/components/cards/Ideas_PrimaryImageCard.svelte';
+	import IdeasSecondaryImageCard from '$lib/components/cards/Ideas_SecondaryImageCard.svelte';
+
+	let { title, author, date, excerpt, img, slug } = $props();
+</script>
+
+<div>
+	<p>{title}</p>
+	<p>{author}</p>
+	<p>{date}</p>
+	<p>{excerpt}</p>
+	<p>{img}</p>
+	<p>{slug}</p>
+</div>
