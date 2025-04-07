7a2e2137e652be5137e99226bb843595f2d6ac03
Isaac Druin <isaac.druin@gmail.com>
2025-04-06 19:53:08 -0500

    add form validation

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -3,53 +3,93 @@ import { Resend } from 'resend';
 import { RESEND } from '$env/static/private';
 import type { Actions } from './$types';
 import slugify from 'slugify';
+import { z } from 'zod';
+import { fail } from '@sveltejs/kit';
 
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
-		const name = data.get('name') as string;
-		const email = data.get('email') as string;
-		const message = data.get('message') as string;
+
+		const rawForm = {
+			name: data.get('name'),
+			email: data.get('email'),
+			message: data.get('message')
+		};
 		const subscriber = data.get('subscriber') === 'on';
 
-		const mutations: any = [
-			{
-				create: {
-					_type: 'formResponse',
-					name,
-					email,
-					message,
-					newsletterSubscriber: subscriber
-				}
-			}
-		];
-
-		// If they want to subscribe, also add to the newsletterSubscriber collection
-		if (subscriber) {
-			mutations.push({
-				createIfNotExists: {
-					_type: 'newsletterSubscriber',
-					// Use the email as the _id if you want to de-dupe by email
-					// You can also use a custom slug or hashed email if needed
-					_id: `emailsubscriber.${slugify(email, { remove: /[*+~.()'"!:@]/g, lower: true })}`,
-					name,
-					email
-				}
-			});
-		}
+		const contactFormSchema = z.object({
+			name: z
+				.string({
+					required_error: 'Name is required.'
+				})
+				.min(2, 'Name must be at least 2 characters.')
+				.max(65, 'Name must be under 65 characters.'),
+			email: z
+				.string({
+					required_error: 'Email is required.'
+				})
+				.email('Please enter a valid email address.'),
+			message: z
+				.string({
+					required_error: 'Message is required.'
+				})
+				.min(10, 'Message must be at least 10 characters.')
+				.max(2000, 'Message may not exceed 2000 characters.')
+		});
 
 		try {
-			const result = await readWriteClient.mutate(mutations);
+			const zParse = contactFormSchema.safeParse(rawForm);
+
+			if (!zParse.success) {
+				return fail(400, {
+					error: 'Validation failed',
+					errors: {
+						...zParse.error.flatten().fieldErrors,
+						data: { ...rawForm, subscriber: subscriber }
+					}
+				});
+			}
+
+			const { name, email, message } = zParse.data;
+
+			const mutations: any = [
+				{
+					create: {
+						_type: 'formResponse',
+						name,
+						email,
+						message,
+						newsletterSubscriber: subscriber
+					}
+				}
+			];
+
+			// If they want to subscribe, also add to the newsletterSubscriber collection
+			if (subscriber) {
+				mutations.push({
+					createIfNotExists: {
+						_type: 'newsletterSubscriber',
+						// use the email as the id to prevent dupes
+						_id: `emailsubscriber.${slugify(email, { remove: /[*+~.()'"!:@]/g, lower: true })}`,
+						name,
+						email
+					}
+				});
+			}
+
+			const mutationResult = await readWriteClient.mutate(mutations);
+			console.log('Mutations successful:', mutationResult);
+
 			const resend = new Resend(RESEND);
 			const emailHtml = `
 					<div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
 						<h2 style="color: #2c3e50;">📬 New Form Response</h2>
-						<hr style="border: 1px solid #eaeaea;">
+						<hr style="border: 1px solid #eaeaea;" />
 						<p><strong>Name:</strong> ${name}</p>
 						<p><strong>Email:</strong> <a href="mailto:${email}" style="color: #3498db;">${email}</a></p>
-						<p><strong>Message:</strong><br>${message}</p>
+						<p><strong>Message:</strong><br />${message}</p>
 						<p><strong>Subscribed to Newsletter:</strong> ${subscriber ? '✅ Yes' : '❌ No'}</p>
-						<hr style="border: 1px solid #eaeaea;">
+						<hr style="border: 1px solid #eaeaea;" />
 						<footer style="font-size: 0.9em; color: #777;">
 							<p>This message was sent from your website form.</p>
 						</footer>
@@ -62,12 +102,14 @@ export const actions = {
 				subject: 'New Form Response',
 				html: emailHtml
 			});
-
-			console.log('Mutations successful:', result);
-			console.log('Email successful:', data);
-			console.log('Email errors', error);
+			if (!error) {
+				console.log('Email successful:', data);
+			} else {
+				console.log('Email errors', error);
+			}
+			return { success: true };
 		} catch (err) {
-			console.error('Mutation failed:', err.message);
+			console.error('Something went wrong:', err.message);
 		}
 	}
 } satisfies Actions;
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,16 +6,9 @@
 	import meta500 from '$lib/assets/images/meta/meta-square-500.webp';
 	//
 	import { enhance } from '$app/forms';
-	import { z } from 'zod';
 	import SectionHeader from '$lib/components/blocks/SectionHeader.svelte';
 	import BtnSolid from '$lib/components/buttons/Btn_Solid.svelte';
-
-	const schema = z.object({
-		name: z.string().min(1, { message: 'Name is required' }),
-		email: z.string().email({ message: 'Invalid email address' }),
-		message: z.string().min(1, { message: 'Message is required' }),
-		subscriber: z.boolean().optional()
-	});
+	let { form } = $props();
 </script>
 
 <Meta
@@ -29,62 +22,91 @@
 />
 <SectionHeader title="Contact Us" body="Get in touch with us!"></SectionHeader>
 
-<div class="sectionMarginX z-[1000] -mt-16 mb-16">
-	<form
-		method="POST"
-		use:enhance
-		class="bg-brand-light dark:bg-brand-teal-med flex w-full flex-col items-center space-y-4 rounded-sm px-10 py-6 shadow-lg lg:px-20 lg:py-14"
-	>
-		<h1 class="w-full text-3xl font-bold">Send Us A Message</h1>
-		<p class="w-full">
-			We would love to hear from you! Use the form below to send us a message. Alternitively, you
-			can send us a good ol' fashioned email, give us a call, or connect with us on social media.
-		</p>
-		<div class="flex w-full flex-wrap items-start gap-8 lg:flex-row lg:space-y-0 lg:space-x-5">
-			<div>
-				<label for="email">Name:</label>
-				<input
-					name="name"
-					placeholder="John Doe"
-					autocomplete="given-name"
-					class="border-brand-teal-med w-full rounded-sm border-2 px-5 py-3"
-					type="text"
-				/>
+<div class="w-full px-8">
+	<div class="z-[1000] container mx-auto -mt-16 mb-16">
+		<form
+			method="POST"
+			use:enhance
+			class="bg-brand-light dark:bg-brand-teal-med flex w-full flex-col items-center space-y-4 rounded-sm px-10 py-6 shadow-lg lg:px-20 lg:py-14"
+		>
+			<h1 class="w-full text-3xl font-bold">Send Us A Message</h1>
+			<p class="w-full">
+				We would love to hear from you! Use the form below to send us a message. Alternitively, you
+				can send us a good ol' fashioned email, give us a call, or connect with us on social media.
+			</p>
+			<div class="flex w-full flex-wrap items-start gap-8 lg:space-y-0 lg:space-x-5">
+				<div class="w-fit">
+					<label for="email">Name:</label>
+					<input
+						name="name"
+						placeholder="John Doe"
+						autocomplete="given-name"
+						class="{form?.errors?.name
+							? 'border-red-600'
+							: 'border-brand-teal-med'} w-full rounded-sm border-2 px-5 py-3"
+						type="text"
+						value={form?.errors?.data ? form.errors.data.name : ''}
+					/>
+					{#if form?.errors?.name}
+						<p class="mt-1 text-sm text-red-600">{form.errors.name[0]}</p>
+					{/if}
+				</div>
+
+				<div class="w-fit">
+					<label for="email">Email:</label>
+					<input
+						class="{form?.errors?.email
+							? 'border-red-600'
+							: 'border-brand-teal-med'} w-full rounded-sm border-2 px-5 py-3"
+						type="text"
+						name="email"
+						placeholder="john.doe@email.com"
+						autocomplete="email"
+						value={form?.errors?.data ? form.errors.data.email : ''}
+					/>
+					{#if form?.errors?.email}
+						<p class="mt-1 text-sm text-red-600">{form.errors.email[0]}</p>
+					{/if}
+					<!-- <span class="text-brand-orange w-fit text-sm font-bold {form?.errors.email ? '' : 'hidden'}"
+					>{form?.errors.email}</span
+				> -->
+				</div>
 			</div>
 
-			<div>
-				<label for="email">Email:</label>
-				<input
-					class="border-brand-teal-med w-full rounded-sm border-2 px-5 py-3"
-					type="text"
-					name="email"
-					placeholder="john.doe@email.com"
-					autocomplete="email"
-				/>
+			<div class="flex w-full flex-col">
+				<label class="w-full" for="message">Message:</label>
+				<textarea
+					class="{form?.errors?.message
+						? 'border-red-600'
+						: 'border-brand-teal-med'} w-full rounded-sm border-2 px-5 py-3"
+					name="message"
+					value={form?.errors?.data && typeof form.errors.data.message === 'string'
+						? form.errors.data.message
+						: ''}
+					cols="30"
+					rows="10"
+				></textarea>
+				{#if form?.errors?.message}
+					<p class=" mt-1 text-sm text-red-600">{form.errors.message[0]}</p>
+				{/if}
 			</div>
-		</div>
 
-		<div class="flex w-full flex-col items-end space-x-0 lg:space-y-0 lg:space-x-5">
-			<label class="w-full" for="message">Message:</label>
-			<textarea
-				class="border-brand-teal-med w-full rounded-sm border-2 px-5 py-3"
-				name="message"
-				id=""
-				cols="30"
-				rows="10"
-			></textarea>
-		</div>
-
-		<div class="flex items-center space-x-2">
-			<!-- TODO: figure out this input color shit -->
-			<input
-				class="border-brand-teal-med ring-brand-teal-light !accent-brand-teal-dark aspect-square rounded-sm border-2"
-				type="checkbox"
-				name="subscriber"
-			/>
-			<label class="text-xs md:text-sm" for="subscriber">Subscribe to our newsletter?</label>
-		</div>
-
-		<BtnSolid buttonText="Submit" />
-	</form>
+			<div class="flex items-center space-x-2">
+				<!-- TODO: figure out this input color shit -->
+				<input
+					class="border-brand-teal-med ring-brand-teal-light !accent-brand-teal-dark aspect-square rounded-sm border-2"
+					type="checkbox"
+					name="subscriber"
+				/>
+				<label class="text-xs md:text-sm" for="subscriber">Subscribe to our newsletter?</label>
+			</div>
+			{#if form?.success}
+				<h2 class="text-2xl text-green-500">
+					Thank you for reaching out, we will get back to you ASAP!
+				</h2>
+			{:else}
+				<BtnSolid buttonText="Submit" />
+			{/if}
+		</form>
+	</div>
 </div>
