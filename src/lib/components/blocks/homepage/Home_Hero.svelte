64c76144cfb0ae0e5d6465ef8e005f8a7e5e5b8c
Isaac Druin <isaac.druin@gmail.com>
2025-09-04 19:58:25 -0500

    update homepage text

--- src/lib/components/blocks/homepage/Home_Hero.svelte
+++ src/lib/components/blocks/homepage/Home_Hero.svelte
@@ -36,12 +36,12 @@
 		{/if}
 	</div>
 	<div
-		class="sm:-translate-y-25 container mx-auto flex h-[90vh] max-w-lg -translate-y-16 flex-col items-center justify-center gap-8 sm:h-[80vh] md:h-[60vh] md:min-h-[750px] md:-translate-y-20"
+		class="container mx-auto flex h-[90vh] max-w-lg -translate-y-16 flex-col items-center justify-center gap-8 sm:h-[80vh] sm:-translate-y-25 md:h-[60vh] md:min-h-[750px] md:-translate-y-20"
 	>
 		<h1 class="text-center text-3xl font-bold md:text-4xl lg:text-5xl">Welcome to CAS</h1>
 		<h2 class="text-center font-semibold md:text-lg lg:text-xl">
-			C&A is a full-service consulting and real estate appraisal firm serving Eastern South Dakota,
-			Northeastern Nebraska, and Western Iowa
+			CAS is a full-service consulting and real estate appraisal firm serving Eastern South Dakota,
+			Northeastern Nebraska, Northwestern Iowa, and Southwestern Minnesota
 		</h2>
 		<BtnSolid buttonText="About Us" url="/about"></BtnSolid>
 	</div>
